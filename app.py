02e1f13095b77ac8ca7f5315c9c5a71a53d4b0ab
Mihai-Nicolae Dulgheru <dulgherumihai19@stud.ase.ro>
2025-02-13 18:58:06 +0200

    Update Flask app host and port configuration to use localhost and port 8080

--- app.py
+++ app.py
@@ -79,4 +79,4 @@ def remove_background():
 
 if __name__ == "__main__":
     # Run the Flask app (development mode)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=8080, debug=True)
